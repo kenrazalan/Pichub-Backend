50b244b04a6c7e1d57ee74ecb95bdf3c5258a373
kenrazalan <kennethrazalan29@gmail.com>
2021-02-08 17:09:58 +0800

    jwt

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://kenneth:09295089159@cluster0.ricr8.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://kenneth:09295089159@cluster0.ricr8.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET:"sdfsadsada"
 }
