4cc90accc1d5525b53afcac69d4865d1541c15d6
kenrazalan <kennethrazalan29@gmail.com>
2021-02-09 17:05:51 +0800

    cors

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./keys')
 const PORT = 5000
+const cors = require('cors')
+
 
 
 require('./models/user')
 require('./models/post')
 
+app.use(cors())
 app.use(express.json())
 app.use(require('./routers/auth'))
 app.use(require('./routers/post'))
