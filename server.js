0ae324c0ac6c54cf449387debdd4382e091dd773
kenrazalan <kennethrazalan29@gmail.com>
2021-02-16 14:31:57 +0800

    depen

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./keys')
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const cors = require('cors')
 
 
@@ -27,5 +28,5 @@ mongoose.connection.on('error',(err)=>{
 
 
 app.listen(PORT,()=>{
-    console.log(`Server listen to port ${PORT}`);
+    console.log(`Server listen to port at ${process.env.NODE_ENV}mode at ${PORT}`);
 })
