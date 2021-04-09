14cc393dc78eee7adf8d48d55c3783392e592598
kenneth <kennethrazalan29@gmail.com>
2021-04-09 16:30:50 +0800

    username lowercase

--- models/user.js
+++ models/user.js
@@ -13,7 +13,8 @@ const userSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true,
-        unique:true
+        unique:true,
+        lowercase: true
     },
     password:{
         type:String,
