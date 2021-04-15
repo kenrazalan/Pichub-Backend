c0142007d24d9bb43ad14b5ac647c75029108361
kenneth <kennethrazalan29@gmail.com>
2021-04-15 22:43:22 +0800

    refactor auth to async await

--- routes/auth.js
+++ routes/auth.js
@@ -11,67 +11,57 @@ const nodemailer = require('nodemailer')
 const sendGridTransport = require('nodemailer-sendgrid-transport')
 const {OAuth2Client} = require('google-auth-library')
 
-let transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{  user: process.env.USERNAME, pass: process.env.PASSWORD
-    },
-    tls: { rejectUnauthorized: false }
-})
 
 
 const client = new OAuth2Client("292368699085-jb2puctimlk06qjc65noft4bp2v574bu.apps.googleusercontent.com")
-const emailValidation = '!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
-router.post('/signup',(req,res)=>{
+
+
+router.post('/signup', async (req,res) => {
     const {name,email,password,pic,username} = req.body
     if(!name || !email || !password ||!username){
        return res.status(422).json({error: " Please add all fields"})
     }
-    else if(`${emailValidation}.test(${email})`){
+    else if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
      return res.status(422).json({error: " Invalid Email"})
     }
-    User.findOne({email:email}).then((savedUser)=>{
-        if(savedUser){ return res.status(422).json({error: "Email already taken"}) }
-        User.findOne({username: username})
-        .then((user)=>{
-         if(user){ return res.status(422).json({error: "Username already taken"}) }
-        bcrypt.hash(password,12)
-        .then(hashedPassword => {
-         const user = new User({
-             email,
-             password:hashedPassword,
-             name,
-             pic ,
-             username 
-         })  
-         user.save()
-         .then(user=>{ res.json({message: "Sign up success"}) })
-         .catch(error=>{ console.log(error) })
-        })
-     })
-    }).catch(error=>{  console.log(error) })
- })  
+    const savedUser =  await User.findOne({email:email}).lean().exec()
+    if(savedUser){ return res.status(422).json({error: "Email already taken"}) }
 
-router.post('/signin',(req,res)=>{
-    const {email,name,password} = req.body;
+    const savedUsername =await User.findOne({username: username}).lean().exec()
+    if(savedUsername){ return res.status(422).json({error: "Username already taken"}) }
+
+    const hashedPassword =await bcrypt.hash(password,12)
+    const user = await new User({
+        email,
+        password:hashedPassword,
+        name,
+        pic ,
+        username 
+    })  
+    await user.save()
+    res.status(200).json({message: "Sign up success"})
+   
+})
+
+router.post('/signin', async (req,res) => {
+    const {email,password} = req.body;
     if(!email || !password){
         return res.json({error:"Please provide Email and password"})
     }
-    User.findOne({email})
-     .populate({ path: "followers", select: "pic username name" })
-     .populate({ path: "following", select: "pic username name" })
-     .populate({ path: "savedPosts", select: "likes comments body photo"})
-     .then(savedUser=>{
-        if(!savedUser){ res.json({error:"Invalid email or password"}) }
-        bcrypt.compare(password,savedUser.password)
-        .then(doMatch=>{
-            if(doMatch){
-                //res.json({message:"Successfully Signed in"})
-                const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
-                const {_id,name,email,followers,following,pic,username,savedPosts} =savedUser
-                res.json({token,user:{_id,name,email,followers,following,pic,username,savedPosts}})}
-            else { return res.status(422).json({error: "Invalid Email or Password"}) } })
-            .catch(err => { console.log(err); })
-    })
+    const savedUser =await User.findOne({email})
+    .populate({ path: "followers", select: "pic username name" })
+    .populate({ path: "following", select: "pic username name" })
+    .populate({ path: "savedPosts", select: "likes comments body photo"})
+    .lean().exec()
+    if(!savedUser){ res.json({error:"Invalid email or password"}) }
+    const isDoMatch = await bcrypt.compare(password,savedUser.password)
+    if(isDoMatch){
+        const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+        const {_id,name,email,followers,following,pic,username,savedPosts} =savedUser
+        res.json({token,user:{_id,name,email,followers,following,pic,username,savedPosts}})}
+    else { 
+        return res.status(422).json({error: "Invalid Email or Password"}) 
+    }
 })
 
 router.post('/googlelogin',(req,res)=>{
@@ -110,29 +100,29 @@ router.post('/googlelogin',(req,res)=>{
     })
 })
 
-router.post("/resetpassword",(req,res)=>{
-    crypto.randomBytes(32,(error,buffer)=>{
-        if(error){ console.log(error) }
-        const token = buffer.toString("hex")
-        User.findOne({email: req.body.email})
-        .then((user => {
-            if(!user){ return res.status(422).json({ error: "User dont exist with that email"}) }
-            user.resetToken = token
-            user.expiredToken = Date.now() + 3600000
-            user.save()
-            .then((result)=>{
-                transporter.sendMail({
-                    to: user.email,
-                    from:process.env.USERNAME,
-                    subject: "Password Reset",
-                    html:
-                            `<p>You requested for password reset</p>
-                             <h3>Click this <a href='${EMAIL}/reset/${token}'>link </a>to reset passwrod. </h3>
-                            `
-                })
-                res.json({message:"Please check your email or check spam folder if you don't recieve in inbox"})
-            })
-        }))
-    })
-})
+// router.post("/resetpassword",(req,res)=>{
+//     crypto.randomBytes(32,(error,buffer)=>{
+//         if(error){ console.log(error) }
+//         const token = buffer.toString("hex")
+//         User.findOne({email: req.body.email})
+//         .then((user => {
+//             if(!user){ return res.status(422).json({ error: "User dont exist with that email"}) }
+//             user.resetToken = token
+//             user.expiredToken = Date.now() + 3600000
+//             user.save()
+//             .then((result)=>{
+//                 transporter.sendMail({
+//                     to: user.email,
+//                     from:process.env.USERNAME,
+//                     subject: "Password Reset",
+//                     html:
+//                             `<p>You requested for password reset</p>
+//                              <h3>Click this <a href='${EMAIL}/reset/${token}'>link </a>to reset passwrod. </h3>
+//                             `
+//                 })
+//                 res.json({message:"Please check your email or check spam folder if you don't recieve in inbox"})
+//             })
+//         }))
+//     })
+// })
 module.exports= router
--- routes/post.js
+++ routes/post.js
@@ -4,13 +4,7 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
-// router.get('/myposts',requireLogin,(req,res)=>{
-//     Post.find({postedBy:req.user._id})
-//     .populate("postedBy", "_id name pic username")
-//     .sort('-createdAt')
-//     .then(myposts=>{ res.json({myposts}) })
-//     .catch(err=>{  console.log(err); })
-// })
+
 router.get('/myposts',requireLogin, async (req,res) => {
     const myposts = await Post.find({postedBy: req.user._id})
     .populate("postedBy", "_id name pic username")
@@ -18,14 +12,6 @@ router.get('/myposts',requireLogin, async (req,res) => {
     res.status(200).json({myposts})
 })
 
-
-// router.get('/post/:id',requireLogin,(req,res)=>{
-//     Post.findOne({_id: req.params.id})
-//     .populate("postedBy","_id name pic followers username")
-//     .populate("comments.postedBy","_id name pic username")
-//     .then(post=>{ res.json({post}) })
-//     .catch(error=>{ return res.status(404).json({error}) })
-// })
 
 router.get('/post/:id',requireLogin, async (req,res)=>{
     const post = await Post.findOne({_id: req.params.id})
@@ -35,15 +21,6 @@ router.get('/post/:id',requireLogin, async (req,res)=>{
     res.status(200).json({post})
 })
 
-// router.get('/followingpost',requireLogin,(req,res)=>{
-//     //return post by user following
-//     Post.find({postedBy:{$in:[...req.user.following,req.user._id]}})
-//     .populate("postedBy","_id name pic followers username")
-//     .populate("comments.postedBy","_id name pic username")
-//     .sort('-createdAt')
-//     .then(posts=>{ res.json({posts}) })
-//     .catch((error)=>{ console.log(error) })
-// })
 
 router.get('/followingpost',requireLogin, async(req,res)=>{
     const posts = await Post.find({postedBy:{$in:[...req.user.following,req.user._id]}})
@@ -52,15 +29,7 @@ router.get('/followingpost',requireLogin, async(req,res)=>{
        .sort('-createdAt').lean().exec()
         res.status(200).json({posts})
 })
-    
-// router.get('/allpost',requireLogin,(req,res)=>{
-//     Post.find()
-//     .populate("postedBy","_id name pic username")
-//     .populate("comments.postedBy","_id name pic username")
-//     .sort('-createdAt')
-//     .then(posts=>{ res.json({posts}) })
-//     .catch(err=>{ console.log(err); })
-// })
+
 
 router.get('/allpost',requireLogin, async (req,res) => {
      const posts = await Post.find()
@@ -70,19 +39,6 @@ router.get('/allpost',requireLogin, async (req,res) => {
      res.status(200).json({posts})
 })
 
-// router.post('/createpost',requireLogin,(req,res)=>{
-//     const {body,pic} = req.body
-//     if(!pic){ return res.status(422).json({error: "Please add all fields"}) }
-//     req.user.password = undefined
-//     const post = new Post({
-//         body,
-//         photo: pic,
-//         postedBy: req.user
-//     })
-//     post.save()
-//     .then(result => { res.json({post:result}) })
-//     .catch(err => { console.log(err); })
-// })
 
 router.post('/createpost',requireLogin, async (req,res) =>{
     const {body,pic} = req.body
@@ -98,19 +54,6 @@ router.post('/createpost',requireLogin, async (req,res) =>{
 })
 
 
-// router.put("/like",requireLogin,(req,res)=>{
-//     Post.findByIdAndUpdate(req.body.postId,{
-//         $push:{likes:req.user._id}
-//     },{ new:true})
-//     .populate("postedBy","_id name pic followers followings username")
-//     .populate("comments.postedBy","_id name pic followers followings username")
-//     .exec((err,result)=>{
-//         if(err){ return res.status(422).json({error:err})
-//         }else{ res.json(result)
-//         }
-//     }
-//     )})
-
 router.put('/like',requireLogin,async (req,res)=> {
    const like = await Post.findByIdAndUpdate(req.body.postId,
     {  $push:{likes:req.user._id}},{new: true})
@@ -120,15 +63,6 @@ router.put('/like',requireLogin,async (req,res)=> {
     res.status(200).json({like})
 })
 
-    // router.put("/unlike",requireLogin,(req,res)=>{
-    //     Post.findByIdAndUpdate(req.body.postId,{ $pull:{likes:req.user._id} },{ new:true})
-    //     .populate("postedBy","_id name pic followers followings username")
-    //     .populate("comments.postedBy","_id name pic followers followings username")
-    //     .exec((err,result)=>{
-    //         if(err){ return res.status(422).json({error:err}) }
-    //         else{ res.json(result) }
-    //     }
-    //     )})
 
 router.put('/unlike',requireLogin,async (req,res)=> {
     const unlike = await Post.findByIdAndUpdate(req.body.postId,
@@ -140,20 +74,6 @@ router.put('/unlike',requireLogin,async (req,res)=> {
     })
     
     
-//  router.put("/comment",requireLogin,(req,res)=>{
-//     const comment = {
-//         text: req.body.text,
-//         postedBy: req.user._id
-//     }
-//     Post.findByIdAndUpdate(req.body.postId,{
-//         $push:{comments:comment} },{ new:true})
-//             .populate("comments.postedBy","_id name pic followers followings username")
-//             .populate("postedBy","_id name pic followers followings username") 
-//             .exec((err,result)=>{
-//                 if(err){ return res.status(422).json({error:err}) }
-//                 else{ res.json(result) }
-//                 }
-//              )})
 router.put('/comment',requireLogin, async (req,res) => {
     const comment = {
         text : req.body.text,
@@ -167,18 +87,7 @@ router.put('/comment',requireLogin, async (req,res) => {
         res.status(200).json(comments)
 })
 
-    // router.delete('/deletepost/:postId',requireLogin,(req,res)=>{
-    //      Post.findOne({_id: req.params.postId})
-    //             .populate("postedBy","_id")
-    //             .exec((error,post)=>{
-    //             if(!post || error){ return res.status(422).json({error: error})  }
-    //             if(post.postedBy._id.toString() === req.user._id.toString())
-    //             {post.remove()
-    //                  .then(result=>{  res.json(result) })
-    //                  .catch(error=>{  console.log(error)})
-    //             }
-    //     })
-    // })
+
 router.delete('/deletepost/:postId',requireLogin, async (req,res) => {
     const post = await Post.findOne({_id: req.params.postId})
     .populate("postedBy","_id")
