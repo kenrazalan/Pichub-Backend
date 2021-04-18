bf0845f09e01336474172a5c4dd62201d500c232
kenneth <kennethrazalan29@gmail.com>
2021-04-18 19:47:22 +0800

    fix unsave api

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.route('/allusers').get(requireLogin,getAllUsers)
 router.route('/follow').put(requireLogin,follow)
 router.route('/unfollow').put(requireLogin,unfollow)
 router.route('/save').put(requireLogin,save)
-router.route('/unsave').post(requireLogin,unsave)
+router.route('/unsave').put(requireLogin,unsave)
 router.route('/editprofile').put(requireLogin,editprofile)
 router.post('/searchusers', async (req,res) => {
     let userPattern = new RegExp("^"+req.body.query)
